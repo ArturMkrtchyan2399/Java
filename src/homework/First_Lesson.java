35069989ce4ca5c9ee51b9edba08c5c3b5f25d71
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-10-29 00:33:40 +0400

    adding homework

---
+++ src/homework/First_Lesson.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class First_Lesson {
+    public static void main(String[] args) {
+        byte a = 1;
+        System.out.println(a);
+        short b = 2;
+        System.out.println(b);
+        int num = 150;
+        System.out.println(num);
+        float f_num = 12.4F;
+        System.out.println(f_num);
+        double d_num = 12.578;
+        System.out.println(d_num);
+        long l_num = 1658416578L;
+        System.out.println(l_num);
+        char letter = 'A';
+        System.out.println(letter);
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
