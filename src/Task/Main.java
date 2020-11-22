26be0911efdf81e9a369b83f1766fe2a7c49e97e
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2020-11-23 00:10:07 +0400

    homework

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -19,21 +19,21 @@ public class Main {
             students[i] = new Student(current[0], current[1], Integer.parseInt(current[2]), current[3], Double.parseDouble(current[4]));
         }
 
-        StudentService studentService = new StudentService();
+        StudentService ss = new StudentService();
 
         System.out.println("Task 1: Print full names of students");
-        studentService.printFullNames(students);
+        ss.printFullNames(students);
         System.out.println("\nTask 2: Print all male students");
-        studentService.printAllMales(students);
+        ss.printAllMales(students);
         System.out.println("\nTask 3: Print all female students who has mark greater then 50.4");
-        studentService.printAllFemalesGreaterThan50_4(students);
+        ss.printAllFemalesGreaterThan50_4(students);
         System.out.println("\nTask 4: Print student information having the minimal mark");
-        studentService.printStudentWithMinimalMark(students);
+        ss.printStudentWithMinimalMark(students);
         System.out.println("\nTask 5: Print biggest male student information");
-        studentService.printBiggestMaleInformation(students);
+        ss.printBiggestMaleInformation(students);
         System.out.println("\nTask 6: Print students sorted by mark");
-        studentService.printStudentSortedByMark(students);
+        ss.printStudentSortedByMark(students);
         System.out.println("\nTask 7: Print female students sorted by year");
-        studentService.printFemaleSortedByYear(students);
+        ss.printFemaleSortedByYear(students);
     }
 }
